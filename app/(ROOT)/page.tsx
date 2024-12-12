9f40d98f245745b64680a72b85f0fcfee175ae1d
ebuka <ebukaopara180@gmail.com>
2024-12-12 11:31:22 +0100

    implement initial file and folder structure

--- app/(ROOT)/page.tsx
+++ app/(ROOT)/page.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import { TotalBalanceBox } from "@/components/TotalBalanceBox";
+
 
 const Home = () => {
+    const loggedIn = { firstName : 'Ebuka' };
+
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+            <header className='home-header'>
+                <HeaderBox
+                type="greeting"
+                title="Welcome"
+                user={loggedIn?.firstName || "Guest"}
+                subtext= "Access and manage your account and transaction efficiently"
+                />
+
+                <TotalBalanceBox
+                accounts = {[]}
+                totalBanks= {1}
+                totalCurrentBalance={3000.25}
+
+                />
+            </header>
+            
+        </div>
+    </section>
   )
 }
 
